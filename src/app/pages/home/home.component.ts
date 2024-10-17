1cb634f19470f1803d91b6d9892c4169c330c277
Naulet Théo <theo.naulet@gmail.com>
2024-10-17 11:47:19 +0200

    feat(routing): add temporary button to navigate

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Router } from '@angular/router';  // Importer Router
+
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService, private router: Router ) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+  }
+
+  goToDetail(): void {
+    this.router.navigate(['/detail']);
   }
 }
