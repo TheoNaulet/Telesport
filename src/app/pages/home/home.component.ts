a12d382e28f8f0e2a4279f7ddbfd7c6a5c54de10
Naulet Théo <theo.naulet@gmail.com>
2024-10-17 14:57:42 +0200

    feat(dev): add functions to get JOs data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Router } from '@angular/router';
-
+import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics: Array<any> = []; 
+  public numberOfJOs: number = 0;
+  public numberOfCountries: number = 0;
 
-  constructor(private olympicService: OlympicService, private router: Router ) {}
+  constructor(private olympicService: OlympicService, private router : Router) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.getOlympics().subscribe((data) => {
+      this.olympics = data; 
+      this.numberOfJOs = this.getNumberOfJOs(); 
+      this.numberOfCountries = this.getNumberOfCountries();
+    });
   }
 
+  getNumberOfCountries(): number {
+    return this.olympics.length; 
+  }
+
+  getNumberOfJOs(): number {
+    let yearsOfJOs: number[] = [];
+
+    this.olympics.forEach(olympic => {
+      console.log(olympic);
+      olympic.participations.forEach((participation: { year: number }) => {
+        if (!yearsOfJOs.includes(participation.year)) {
+          yearsOfJOs.push(participation.year);
+        }
+      });
+    });
+
+    return yearsOfJOs.length;
+  }
+  
   goToDetail(): void {
     this.router.navigate(['/detail']);
   }
