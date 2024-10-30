8f5ca0fe9da6e3d27e1c61f1a925f14066d73427
Naulet Théo <theo.naulet@gmail.com>
2024-10-30 21:43:42 +0100

    refactor(docs): translate comments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -39,10 +39,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.subscription.add(
       this.olympicService.getOlympics().subscribe((data: Country[] | null) => {
         if (data) {
-          this.olympics = data; // 'data' est typé en 'Olympic[]'
-          this.numberOfJOs = this.getNumberOfJOs(); // Calculer le nombre de JO
-          this.numberOfCountries = this.getNumberOfCountries(); // Calculer le nombre de pays participants
-          this.chartData = this.getChartData(); // Préparer les données pour le graphique
+          this.olympics = data; // 'data' est typé en 'Country[]'
+          this.numberOfJOs = this.getNumberOfJOs(); // Calculate the number of Olympic Games
+          this.numberOfCountries = this.getNumberOfCountries(); // Calculate the number of countries
+          this.chartData = this.getChartData(); // Prepare the data for the pie chart
         }
       })
     );
